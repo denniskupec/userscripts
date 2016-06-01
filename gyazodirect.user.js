fc22b55ee6173bf5d3a1425edf2de9e7a5544f26
Dennis Kupec <denniskupec@gmail.com>
2016-06-01 17:06:56 -0500

    fix incorrect exclude/include order

--- gyazodirect.user.js
+++ gyazodirect.user.js
@@ -5,7 +5,6 @@
 // @run-at       document-start
 // @description  Makes Gyazo screenshots direct links to the image
 // @author       Dennis Kupec
-// @match        https://gyazo.com/*
 // @exclude      https://gyazo.com/search
 // @exclude      https://gyazo.com/verify/*
 // @exclude      https://gyazo.com/teams/*
@@ -13,6 +12,7 @@
 // @exclude      https://gyazo.com/collections
 // @exclude      https://gyazo.com/pricing
 // @exclude      https://gyazo.com/download
+// @match        https://gyazo.com/*
 // @grant        location
 // ==/UserScript==
 
