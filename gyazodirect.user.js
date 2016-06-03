da041f76ec1091b48c124da64f9e44634f525b9d
Dennis Kupec <denniskupec@gmail.com>
2016-06-03 16:37:26 -0500

    update url includes/excludes to allow http/https

--- gyazodirect.user.js
+++ gyazodirect.user.js
@@ -5,14 +5,14 @@
 // @run-at       document-start
 // @description  Makes Gyazo screenshots direct links to the image
 // @author       Dennis Kupec
-// @exclude      https://gyazo.com/search
-// @exclude      https://gyazo.com/verify/*
-// @exclude      https://gyazo.com/teams/*
-// @exclude      https://gyazo.com/settings
-// @exclude      https://gyazo.com/collections
-// @exclude      https://gyazo.com/pricing
-// @exclude      https://gyazo.com/download
-// @match        https://gyazo.com/*
+// @exclude      *://gyazo.com/search
+// @exclude      *://gyazo.com/verify/*
+// @exclude      *://gyazo.com/teams/*
+// @exclude      *://gyazo.com/settings
+// @exclude      *://gyazo.com/collections
+// @exclude      *://gyazo.com/pricing
+// @exclude      *://gyazo.com/download
+// @match        *://gyazo.com/*
 // @grant        location
 // ==/UserScript==
 
