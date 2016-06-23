bb284deec5f48208a9183fb1fb2b1b8026fe34e0
Dennis Kupec <denniskupec@gmail.com>
2016-06-23 15:36:12 -0500

    Update auto-steam-discovery-queue.user.js

--- auto-steam-discovery-queue.user.js
+++ auto-steam-discovery-queue.user.js
@@ -5,6 +5,7 @@
 // @description  Auto run through the discovery queue for Steam Summer Sale 2016 trading cards
 // @author       Dennis Kupec
 // @match        *://store.steampowered.com/app/*
+// @match        *://store.steampowered.com/explore/*
 // @run-at       document-end
 // @grant        document.querySelector
 // @grant        location.reload
@@ -13,16 +14,29 @@
 (function() {
   'use strict';
 
+  // TODO: bypass age verification screens
+
   var error = document.querySelector('body > div:nth-child(2) > p:nth-child(10)');
-  var button = document.querySelector('div.btn_next_in_queue');
+  var btn_next = document.querySelector('div.btn_next_in_queue');
+  var btn_newqueue = document.querySelector('#refresh_queue_btn');
+
+  if(btn_newqueue !== null) {
+    if(document.querySelector('div.discovery_queue_winter_sale_cards_header > div.subtext').innerText == 'Come back tomorrow to earn more cards by browsing your Discovery Queue!') {
+      return;
+    }
+
+    btn_newqueue.click();
+  }
 
   if(error !== null) {
     console.log('Steam returned an error: ' + error.innerText + ' - retrying in 30 seconds.');
     setTimeout(location.reload, 30*1000);
   }
 
-  if(button !== null) {
-    button.click();
+  if(btn_next !== null) {
+    btn_next.click();
   }
+
 })();
+
 
