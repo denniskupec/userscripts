dd6dac7e77bacd434c6e32ff5aa0dc856084126f
Dennis Kupec <denniskupec@gmail.com>
2018-06-28 10:46:49 -0500

    Create cloudapp-selectall.user.js

---
+++ cloudapp-selectall.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name			CloudApp Select All
+// @namespace		http://baud.nu
+// @version			1
+// @description		Select All drops for the new CloudApp UI
+// @author			Dennis Kupec
+// @match			https://app.cl.ly/drops/*
+// @run-at			document-start
+// @grant			GM_registerMenuCommand
+// ==/UserScript==
+
+GM_registerMenuCommand(
+	'select-all',
+	() => document.querySelectorAll('.ItemsList input[type="checkbox"]').forEach((e) => e.checked = !e.checked),
+	'a'
+);
